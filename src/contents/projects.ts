6a551a5dcbef57b16abe40bc7f1f7cd1da0213a7
Hansa Sandamal <hansa.sandamal@hatchyard.io>
2025-11-17 20:14:33 +0530

    fix add new project

--- src/contents/projects.ts
+++ src/contents/projects.ts
@@ -3,6 +3,14 @@ import { Project } from "@/types";
 
 export const projects: Project[] = [
   {
+    title: 'Dish-to-Post AI',
+    description: 'A web app forTurn your menu into viral-ready social media content. Simply upload your dish images, and our AI crafts engaging posts complete with catchy captions and relevant hashtags to boost your online presence.',
+    technologies: ['Google ai Studio', 'FireBase Backend', 'Google Auth', 'Gemini 2.5-flash'],
+    githubLink: 'https://github.com/HansaSandamal/Dish-to-Post-AI.git',
+    demoLink: 'https://dish-to-post-ai.vercel.app/',
+    image: '/projects/dish.png',
+  },
+  {
     title: 'Finance Tracker',
     description: 'A simple and intuitive web app to track income, expenses, and savings. Built with React, TypeScript, and Redux, it helps users categorize transactions, visualize spending patterns, and manage personal finances with ease.',
     technologies: ['Next.js', 'Tailwind CSS', 'react-chartjs-2', 'Redux Toolkit', 'TypeScript', 'react hooks'],
